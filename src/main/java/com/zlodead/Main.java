936fe7377f511c955196880ee1ad81fcd02212b0
Siarhei Arlou <Siarhei.Arlou@itechart-group.com>
2019-10-14 09:51:39 +0300

    reformated code

--- src/main/java/com/zlodead/Main.java
+++ src/main/java/com/zlodead/Main.java
@@ -17,11 +17,11 @@ public class Main {
         double min = array[0];
         double avg = 0;
         for (int i = 0; i < array.length; i++) {
-            if(max < array[i])
+            if (max < array[i])
                 max = array[i];
-            if(min > array[i])
+            if (min > array[i])
                 min = array[i];
-            avg += array[i]/array.length;
+            avg += array[i] / array.length;
         }
 
         System.out.println("Максимальное значение = " + max);
@@ -41,4 +41,4 @@ public class Main {
         System.out.print(Arrays.toString(array));
     }
 
-                 }
+}
